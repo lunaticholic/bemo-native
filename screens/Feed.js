9b9cb02d9b2ef87ba14970326d1a25a6494af683
lunaticholic <lunaticholic@gmail.com>
2021-04-28 01:33:09 +0400

    3.10 Infinite Scrolling Part One - 무한스크롤

--- screens/Feed.js
+++ screens/Feed.js
@@ -7,8 +7,8 @@ import Photo from "../components/Photo";
 import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../fragments";
 
 const FEED_QUERY = gql`
-    query seeFeed {
-        seeFeed {
+    query seeFeed($offset: Int!) {
+        seeFeed(offset: $offset) {
         ...PhotoFragment
         user {
             username
@@ -27,7 +27,11 @@ const FEED_QUERY = gql`
 `;
 
 export default function Feed() {
-    const { data, loading, refetch } = useQuery(FEED_QUERY);
+    const { data, loading, refetch, fetchMore } = useQuery(FEED_QUERY, {
+        variables: {
+            offset: 0,
+        },
+    });
     const renderPhoto = ({ item: photo }) => {
         return <Photo {...photo} />;
     };
@@ -39,7 +43,10 @@ export default function Feed() {
     const [refreshing, setRefreshing] = useState(false);
     return (
         <ScreenLayout loading={loading}>
-            <FlatList refreshing={refreshing} onRefresh={refresh} style={{ width: "100%" }} showsVerticalScrollIndicator={false} data={data?.seeFeed} keyExtractor={(photo) => "" + photo.id} renderItem={renderPhoto} />
+            <FlatList 
+                onEndReachedThreshold={0}
+                onEndReached={() => fetchMore({ variables: { offset: data?.seeFeed?.length } }) }
+                refreshing={refreshing} onRefresh={refresh} style={{ width: "100%" }} showsVerticalScrollIndicator={false} data={data?.seeFeed} keyExtractor={(photo) => "" + photo.id} renderItem={renderPhoto} />
         </ScreenLayout>
     );
 }
