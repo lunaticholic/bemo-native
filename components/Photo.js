469aa4a9dcaef43e06806c0e01f253255137bac9
lunaticholic <lunaticholic@gmail.com>
2021-04-28 01:24:26 +0400

    3.7 Photo Part Two - 사진 띄우고 정렬하기 2번째

--- components/Photo.js
+++ components/Photo.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components/native";
-import { useWindowDimensions } from "react-native";
+import { Image, useWindowDimensions } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 const Container = styled.View``;
-const Header = styled.View``;
-const UserAvatar = styled.Image``;
+const Header = styled.TouchableOpacity`
+    padding: 10px;
+    flex-direction: row;
+    align-items: center;
+`;
+const UserAvatar = styled.Image`
+    margin-right: 10px;
+    width: 25px;
+    height: 25px;
+    border-radius: 12.5;
+`;
 const Username = styled.Text`
     color: white;
+    font-weight: 600;
 `;
 const File = styled.Image``;
 const Actions = styled.View``;
@@ -21,14 +33,21 @@ const Likes = styled.Text`
 `;
 
 function Photo({ id, user, caption, file, isLiked, likes }) {
+    const navigation = useNavigation();
     const { width, height } = useWindowDimensions();
+    const [imageHeight, setImageHeight] = useState(height - 450);
+    useEffect(() => {
+        Image.getSize(file, (width, height) => {
+        setImageHeight(height / 3);
+        });
+    }, [file]);
     return (
         <Container>
-        <Header>
-            <UserAvatar />
+        <Header onPress={() => navigation.navigate("Profile")}>
+            <UserAvatar resizeMode="cover" source={{ uri: user.avatar }} />
             <Username>{user.username}</Username>
         </Header>
-        <File style={{ width, height: height - 500 }} source={{ uri: file }} />
+        <File style={{ width, height: imageHeight }} source={{ uri: file }} />
         <Actions>
             <Action />
             <Action />
@@ -37,7 +56,9 @@ function Photo({ id, user, caption, file, isLiked, likes }) {
             {likes === 1 ? "1 like" : `${likes} likes`}
         </Likes>
         <Caption>
-            <Username>{user.username}</Username>
+            <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+                <Username>{user.username}</Username>
+            </TouchableOpacity>
             <CaptionText>{caption}</CaptionText>
         </Caption>
         </Container>
