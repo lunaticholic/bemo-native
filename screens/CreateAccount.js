720cdafe9d635f72092acc5f4e96d62780a26b81
lunaticholic <lunaticholic@gmail.com>
2021-04-27 22:18:08 +0400

    2.7 Create Account Part Two - 회원가입 폼 만들기

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,19 +1,18 @@
 import React from "react";
+import { TextInput } from "react-native";
 import styled from "styled-components/native"
-import { colors } from "../colors";
-import { TouchableOpacity, View } from "react-native";
-
-const Container = styled.View`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-    background-color: black;
-`;
+import AuthButton from "../components/auth/AuthButton";
+import AuthLayout from "../components/auth/AuthLayout";
 
 export default function CreateAccount() {
     return (
-        <Container>
-            
-        </Container>
+        <AuthLayout>
+            <TextInput placeholder="USERNAME" placeholderTextColor="gray" returnKeyType="next" style={{ backgroundColor: "white", width: "100%"}} />
+            {/* keyboardType을 email-address로 하면 키보드가 자동으로 email입력방식으로 바뀐다. */}
+            <TextInput placeholder="EMAIL" placeholderTextColor="gray" keyboardType="email-address" returnKeyType="next" style={{ backgroundColor: "white", width: "100%"}} />
+            {/* secureTextEntry로 하게 되면 비밀번호처럼 보여주고, 첫 문자를 대문자로 입력하는걸 방지해준다. */}
+            <TextInput placeholder="PASSWORD" placeholderTextColor="gray" secureTextEntry returnKeyType="done" style={{ backgroundColor: "white", width: "100%"}} />
+            <AuthButton text="Create Account" disabled={true} onPress={() => null}/>
+        </AuthLayout>
     )
 }
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,55 +2,28 @@ import React from "react";
 import styled from "styled-components/native"
 import { colors } from "../colors";
 import { TouchableOpacity, View } from "react-native";
-
-const Container = styled.View`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-    background-color: black;
-`;
-const Logo = styled.Image`
-    max-width: 70%;
-`;
-
-const CreateAccount = styled.TouchableOpacity`
-    background-color: ${colors.blue};
-    padding: 7px 10px;
-    border-radius: 7px;
-    position: relative;
-    top: 220px;
-    opacity: ${(props) => props.disabled ? "0.5" : "1"};
-`;
-const CreateAccountText = styled.Text`
-    color: white;
-    font-weight: 700;
-    font-size: 16px;
-`;
+import AuthLayout from "../components/auth/AuthLayout";
+import AuthButton from "../components/auth/AuthButton";
 
 const LoginLink = styled.Text`
     color: ${colors.blue};
     font-weight: 700;
     position: relative;
-    top: 260px;
-    font-size: 16px;
+    top: 50px;
+    font-size: 20px;
 `;
 
 export default function Welcome({ navigation }) {
     const goToCreateAccount = () => navigation.navigate("CreateAccount");
     const goToLogin = () => navigation.navigate("LogIn");
     return (
-        <Container>
-            <Logo resizeMode="contain" source={require("../assets/login_logo.png")} />
-            <CreateAccount disabled={false} onPress={goToCreateAccount}>
-                <CreateAccountText>
-                    Create Account
-                </CreateAccountText>
-            </CreateAccount>
+        <AuthLayout>
+            <AuthButton text="Create Account" disabled={false} onPress={goToCreateAccount} />
             <TouchableOpacity onPress={goToLogin}>
                 <LoginLink>
                     Log In
                 </LoginLink>
             </TouchableOpacity>
-        </Container>
+        </AuthLayout>
     )
 }
