6e3e67dd2d8a35960005f40f636f24fd00b2b1cb
lunaticholic <lunaticholic@gmail.com>
2021-04-28 13:02:50 +0400

    5.15 Upload Part Four - 그만~~~

--- apollo.js
+++ apollo.js
@@ -3,6 +3,7 @@ import { setContext } from "@apollo/client/link/context";
 import { offsetLimitPagination } from "@apollo/client/utilities";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { onError } from "@apollo/client/link/error";
+import { createUploadLink } from "apollo-upload-client";
 
 export const isLoggedInVar = makeVar(false);
 // 토큰이 발급될 떄마다 reactive variable 상에서 token을 사용할 수 있도록 준비가 되어 있다는 뜻
@@ -27,7 +28,7 @@ export const logUserOut = async () => {
 // 니가 지금 핸드폰으로 사진 찍는걸 테스트 하고 싶으면 터미널 하나 열어
 // npx localtunnel --port 4000을 치면 주소 하나가 나올거야
 // 그 주소를 아래 /graphql전까지 바꿔야만 테스트할 수 있어
-const httpLink = createHttpLink({
+const uploadHttpLink = createUploadLink({
     uri: "https://polite-crab-54.loca.lt/graphql",
 });
 
@@ -51,6 +52,6 @@ const onErrorLink = onError(({ graphQLErrors, networkError }) => {
 
 export const cache = new InMemoryCache({ typePolicies: { Query: { fields: { seeFeed: offsetLimitPagination() } } } } );
 
-const client = new ApolloClient({ link: authLink.concat(onErrorLink).concat(httpLink), cache });
+const client = new ApolloClient({ link: authLink.concat(onErrorLink).concat(uploadHttpLink), cache });
 
 export default client;
--- screens/UploadForm.js
+++ screens/UploadForm.js
@@ -44,7 +44,20 @@ const HeaderRightText = styled.Text`
 `;
 
 export default function UploadForm({ route, navigation }) {
-    const [uploadPhotoMutation, { loading, error }] = useMutation(UPLOAD_PHOTO_MUTATION);
+    const updateUploadPhoto = (cache, result) => {
+        const { data: { uploadPhoto } } = result;
+        if (uploadPhoto.id) {
+            cache.modify({ id: "ROOT_QUERY",
+                fields: {
+                    seeFeed(prev) {
+                        return [uploadPhoto, ...prev];
+                    },
+                },
+            });
+            navigation.navigate("Tabs");
+        }
+    };
+    const [uploadPhotoMutation, { loading }] = useMutation( UPLOAD_PHOTO_MUTATION, { update: updateUploadPhoto } );
     const HeaderRight = () => (
         <TouchableOpacity onPress={handleSubmit(onValid)}>
             <HeaderRightText>Next</HeaderRightText>
@@ -60,7 +73,6 @@ export default function UploadForm({ route, navigation }) {
         const file = new ReactNativeFile({ uri: route.params.file, name: `1.jpg`, type: "image/jpeg" });
         uploadPhotoMutation({ variables: { caption, file } });
     };
-    console.log(error);
     return (
         <DismissKeyboard>
         <Container>
